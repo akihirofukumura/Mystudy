f6229ff9ef31fd10f57a3585baf1fc25c9766ee8
akihirofukumura <104131582+akihirofukumura@users.noreply.github.com>
2022-07-27 01:24:16 +0900

    hamburgermenue

--- クリエイティブワーク/js/script.js
+++ クリエイティブワーク/js/script.js
@@ -9,21 +9,22 @@ const hand = document.querySelector(".Imghandshake");
 const handTop = hand.offsetTop;
 
 
-window.addEventListener("scroll",(evt) => {
+window.addEventListener("scroll", (evt) => {
     // console.log(window.scrollY);
-    
-    if(window.scrollY > coffeeTop - stepY){
+
+    if (window.scrollY > coffeeTop - stepY) {
         coffee.classList.add("fade");
     }
-    if(window.scrollY > humTop  - stepY){
+    if (window.scrollY > humTop - stepY) {
         hum.classList.add("fade");
     }
-    if(window.scrollY > convTop  - stepY){
+    if (window.scrollY > convTop - stepY) {
         conv.classList.add("fade");
     }
-    if(window.scrollY > handTop  - stepY){
+    if (window.scrollY > handTop - stepY) {
         hand.classList.add("fade");
     }
 
-    
+
 })
+
