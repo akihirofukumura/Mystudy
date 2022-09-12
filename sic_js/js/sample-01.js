008081e0b3d03d5e30f2fa27f93777c3381d21da
akihirofukumura <104131582+akihirofukumura@users.noreply.github.com>
2022-09-12 10:43:37 +0900

    Math(round)

--- sic_js/js/sample-01.js
+++ sic_js/js/sample-01.js
@@ -1,3 +1,20 @@
 console.log("Hello World.");
 
 console.log(100+14);
+
+//数字と数値の足し算
+// console.jog("100"+14);
+
+console.log(true);
+
+// 計算プログラム
+console.log(100/3);
+
+console.log((100+2)*10);
+
+// 100/3 = 33 あまり 3
+// 小数点の扱い＞小数点以下は切り捨て
+// Math > 算術を提供
+console.log(Math.round(100/3))
+
+console.log("100/3=33余り3")
