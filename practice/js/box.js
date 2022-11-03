8bd4e6409a33c481a3b7ff1aff72af42f43cb405
akihirofukumura <104131582+akihirofukumura@users.noreply.github.com>
2022-11-04 00:12:02 +0900

    mouse move 確認

--- practice/js/box.js
+++ practice/js/box.js
@@ -1,2 +1,9 @@
 const container = document.getElementById("container");
 const box = document.getElementById("box");
+console.log(container);
+
+container.addEventListener("mousemove",function(e){
+  const x = e.clientX;
+  const y = e.clientY;
+  console.log("動いた");
+})
