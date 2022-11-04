158f89ac464ec8361dd211141bb3f3ccce48ad49
akihirofukumura <104131582+akihirofukumura@users.noreply.github.com>
2022-11-04 10:05:28 +0900

    完成
    模写だからもう一度作る

--- practice/js/box.js
+++ practice/js/box.js
@@ -1,9 +1,28 @@
 const container = document.getElementById("container");
 const box = document.getElementById("box");
-console.log(container);
+let isMauseDown = false;
+
+box.addEventListener("mousedown",function() {
+  isMauseDown = true;
+})
+
+box.addEventListener("mouseup",function () {
+  isMauseDown = false;
+})
 
 container.addEventListener("mousemove",function(e){
   const x = e.clientX;
   const y = e.clientY;
   console.log("動いた");
+
+
+if(isMauseDown){
+  box.style.top = `${y-50}px`
+  box.style.left = `${x-50}px`
+} 
+
 })
+
+
+console.log( container.addEventListener("mousemove",function(e){
+}));
