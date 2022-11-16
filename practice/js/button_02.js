3d80c3bfbbe8cb3ef3a49935a40cd5940accbb82
akihirofukumura <104131582+akihirofukumura@users.noreply.github.com>
2022-11-16 13:13:52 +0900

    button 完成

--- practice/js/button_02.js
+++ practice/js/button_02.js
@@ -1,1 +1,24 @@
-console.dir("aaa");
+const names = document.getElementById("name");
+const decide = document.getElementById("decide");
+const clear = document.getElementById("clear");
+const yname = document.querySelector(".yourName");
+
+decide.addEventListener("click", () => {
+  yname.innerHTML += names.value;
+});
+
+clear.addEventListener("click", () => {
+  yname.innerHTML = "";
+});
+
+// function htmlChange(e) {
+//   yname.innerHTML += e;
+// }
+
+// decide.addEventListener("click", () => {
+//   htmlChange(names.value);
+// });
+
+// clear.addEventListener("click", () => {
+// yname.innerHTML = "";
+// });
