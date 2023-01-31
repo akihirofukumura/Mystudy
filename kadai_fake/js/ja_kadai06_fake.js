804bf8cfead7fe78b58d184b2e0d8ff434b6fe8c
akihirofukumura <104131582+akihirofukumura@users.noreply.github.com>
2023-01-31 20:53:26 +0900

    画像の取得
    querySelectorAllを使うことで画像の要素を配列ですべて取得できる

--- kadai_fake/js/ja_kadai06_fake.js
+++ kadai_fake/js/ja_kadai06_fake.js
@@ -2,3 +2,6 @@
 const next = document.getElementById("next");
 //前に戻るボタンの取得
 const prev = document.getElementById("prev");
+// 画像を取得
+const img = document.querySelectorAll("#slider li img");
+console.log(img);
