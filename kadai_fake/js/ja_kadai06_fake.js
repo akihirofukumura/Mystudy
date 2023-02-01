5fd627693b2e7ae5c6ed77e897002ff57f791c6f
akihirofukumura <104131582+akihirofukumura@users.noreply.github.com>
2023-02-01 09:40:41 +0900

    imgの最大枚数 取得
    lengthで最大枚数を取れる

--- kadai_fake/js/ja_kadai06_fake.js
+++ kadai_fake/js/ja_kadai06_fake.js
@@ -4,6 +4,7 @@ const next = document.getElementById("next");
 const prev = document.getElementById("prev");
 // 画像を取得
 const img = document.querySelectorAll("#slider li img");
-console.log(img);
+console.log(img.length);
+//画像の最大枚数
 
 next.addEventListener("click", () => {});
