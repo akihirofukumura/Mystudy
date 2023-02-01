834c8d5c6c05def2a6e126ca7d1e3058d5298bbf
akihirofukumura <104131582+akihirofukumura@users.noreply.github.com>
2023-02-01 10:15:55 +0900

    0に戻す箱num 作成

--- kadai_fake/js/ja_kadai06_fake.js
+++ kadai_fake/js/ja_kadai06_fake.js
@@ -4,7 +4,9 @@ const next = document.getElementById("next");
 const prev = document.getElementById("prev");
 // 画像を取得
 const img = document.querySelectorAll("#slider li img");
-console.log(img.length);
-//画像の最大枚数
+//画像の最大枚数 img.length
 
+//箱をつくる。0にすることで最大枚数に達したときにも戻ってくる
+let num = 0;
+//クリックイベント
 next.addEventListener("click", () => {});
