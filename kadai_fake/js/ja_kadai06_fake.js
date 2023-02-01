39224476f7a815f98091ce32271f59fc40c2cbe5
akihirofukumura <104131582+akihirofukumura@users.noreply.github.com>
2023-02-01 11:56:06 +0900

    完成

--- kadai_fake/js/ja_kadai06_fake.js
+++ kadai_fake/js/ja_kadai06_fake.js
@@ -2,11 +2,30 @@
 const next = document.getElementById("next");
 //前に戻るボタンの取得
 const prev = document.getElementById("prev");
-// 画像を取得
+// 画像すべてを取得
 const img = document.querySelectorAll("#slider li img");
+// 消えてる画像の取得
+const imgWrap = document.querySelectorAll("#slider li");
 //画像の最大枚数 img.length
 
 //箱をつくる。0にすることで最大枚数に達したときにも戻ってくる
 let num = 0;
 //クリックイベント
-next.addEventListener("click", () => {});
+next.addEventListener("click", () => {
+  imgWrap[num].style.display = "none";
+  if (num < img.length - 1) {
+    num += 1;
+  } else {
+    num = 0;
+  }
+  imgWrap[num].style.display = "block";
+});
+prev.addEventListener("click", () => {
+  imgWrap[num].style.display = "none";
+  if (num > 0) {
+    num -= 1;
+  } else {
+    num = img.length - 1;
+  }
+  imgWrap[num].style.display = "block";
+});
