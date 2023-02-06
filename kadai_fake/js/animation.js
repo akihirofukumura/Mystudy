ed482eb2580d55210029eeaeaf5ce9c5042b06c2
akihirofukumura <104131582+akihirofukumura@users.noreply.github.com>
2023-02-07 00:50:32 +0900

    HTML見た目  animation
    ・text入力
    ・ボタン

--- kadai_fake/js/animation.js
+++ kadai_fake/js/animation.js
@@ -1,10 +1,10 @@
 // keyframeで背景色を変える時はグラデーションのような変化になるので近い色にする必要がある
-const keyframes = {
-  background: ["#fcc"],
-  background: ["#f66"],
-};
+// const keyframes = {
+//   background: ["#fcc"],
+//   background: ["#f66"],
+// };
 
 // jsのAnimate( , )には第一引数にkeyframeつまり動かす内容。第二関数には動きの詳細（タイミングなど）
 
 // 暗い赤色から明るくしていく;
-document.body.animate(keyframes, 3000);
+// document.body.animate(keyframes, 3000);
