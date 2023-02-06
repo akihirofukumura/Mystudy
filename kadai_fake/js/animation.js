fa14e6467c45952bf16a8c51867c3f29f7a5ca62
akihirofukumura <104131582+akihirofukumura@users.noreply.github.com>
2023-02-07 02:01:47 +0900

    マウス押した（要素にまだ乗っている）時 animation
    マウスを長押ししてあときにだけしか反応しないので離してからも反応させたい。

--- kadai_fake/js/animation.js
+++ kadai_fake/js/animation.js
@@ -1,10 +1,23 @@
 // keyframeで背景色を変える時はグラデーションのような変化になるので近い色にする必要がある
-// const keyframes = {
-//   background: ["#fcc"],
-//   background: ["#f66"],
-// };
 
 // jsのAnimate( , )には第一引数にkeyframeつまり動かす内容。第二関数には動きの詳細（タイミングなど）
 
 // 暗い赤色から明るくしていく;
-// document.body.animate(keyframes, 3000);
+
+const bgColor = document.getElementById("bgColor");
+const btn = document.getElementById("btn");
+const keyframes = {
+  background: ["#fcc"],
+  background: ["#b22"],
+};
+
+// const colorAnimation = bgColor.animate(keyframes, 3000);
+
+// console.log(colorAnimation);/animation_js.html
+// console.log(bgColor);
+btn.addEventListener("mousedown", () => {
+  bgColor.animate(keyframes, 3000);
+});
+btn.addEventListener("mouseup", () => {
+  bgColor.animate(keyframes, 3000);
+});
