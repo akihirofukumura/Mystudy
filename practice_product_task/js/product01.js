3fba594accad243e788418ae75886dac5bf916ca
akihirofukumura <104131582+akihirofukumura@users.noreply.github.com>
2023-04-28 19:14:59 +0900

    make arrangement. get all boxes.
    QuerySelectorAll property can get the all elements.
    And, When if QuerySelectorAll used that element can be arrangement.

--- practice_product_task/js/product01.js
+++ practice_product_task/js/product01.js
@@ -1,2 +1,9 @@
 const btn = document.getElementById("btn");
-const boxes = document.getElementById("b_wrap");
+const box_wrap = document.getElementById("b_wrap");
+const boxes = document.querySelectorAll(".box");
+
+console.log(boxes.length);
+
+btn.addEventListener("click", () => {
+  for (let i = 0; i < boxes.length; i++) {}
+});
