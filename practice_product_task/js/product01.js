2cd307cf647b80eb2646ec36ec043410e0120d1c
akihirofukumura <104131582+akihirofukumura@users.noreply.github.com>
2023-05-02 18:01:50 +0900

    give up

--- practice_product_task/js/product01.js
+++ practice_product_task/js/product01.js
@@ -1,11 +1,11 @@
 const btn = document.getElementById("btn");
 const box_wrap = document.getElementById("b_wrap");
 const boxes = document.querySelectorAll(".box");
-console.log(boxes);
 
 btn.addEventListener("click", () => {
-  if (boxes.length > 0) {
-    boxes[3].style.display = "none";
-    console.log(boxes);
-  }
+  boxes.shift();
+  // for (let i = 0; i < boxes.length; i++) {
+  //   boxes[i].remove();
+  // }
+  console.log(typeof boxes);
 });
