26a7010b7d323d5bbdbdd6dba17589d958239266
akihirofukumura <104131582+akihirofukumura@users.noreply.github.com>
2023-05-09 23:25:24 +0900

    made delete button and task

--- practice_product_task/js/to-do.js
+++ practice_product_task/js/to-do.js
@@ -3,4 +3,27 @@ const taskValue = document.getElementsByClassName("task_value")[0];
 const taskSubmit = document.getElementsByClassName("task_submit")[0];
 const taskList = document.getElementsByClassName("task_list")[0];
 
-console.log(taskValue);
+// 追加（Addボタン）の機能を実施
+const addTasks = (task) => {
+  // 入力したタスクを追加・表示
+  const listItem = document.createElement("li");
+  const showItem = document.appendChild(listItem);
+  showItem.innerHTML = task;
+
+  // 入力したタスクを削除するためのボタン
+  const deleteButton = document.createElement("button");
+  deleteButton.innerHTML = "Delete";
+  listItem.appendChild(deleteButton);
+
+  // 削除ボタンをクリックした時のイベント
+  deleteButton.addEventListener("click", (evt) => {
+    evt.preventDefault();
+    deleteTasks(deleteButton);
+  });
+
+  // 削除ボタンに機能をつける
+  const deleteTasks = (deleteButton) => {
+    const chosenTask = deleteButton.closest("li");
+    taskList.removeChild(chosenTask);
+  };
+};
